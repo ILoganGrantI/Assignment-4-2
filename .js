b03d667b902f832bf0ab01ee2ecac4ec3a1b976a
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-22 12:35:52 +0000

    Assignment-4-2

---
+++ .js
@@ -0,0 +1,70 @@
+document.getElementById('b1').addEventListener('click', change)
+
+function change () {
+  document.getElementById('paragraph').innerHTML = '<p>21. Disney<br><br>“Disney bought Pixar for $7.4 Billion in 2006. Jobs became a member of Disney’s board and the largest shareholder of the company” (Stieg).</p>'
+  document.getElementById('right-img').src = './Images/21. Disney.JPG'
+}
+// --------------------------1
+document.getElementById('b2').addEventListener('click', change1)
+
+function change1 () {
+  document.getElementById('paragraph').innerHTML = '<p>22. Bill Gates and Steve Jobs Become Friends<br><br>2007 Gates appeared with Jobs on the “AllThingsD conference” (Hartmans, Weinberger), where the two actually expressed admiration for each other.</p>'
+  document.getElementById('right-img').src = './Images/22. Bill Gates and Steve Jobs Become Friends.JPG'
+}
+// --------------------------2
+document.getElementById('b3').addEventListener('click', change2)
+
+function change2 () {
+  document.getElementById('paragraph').innerHTML = '<p>23. iTunes<br><br>“In 2008, Apple became the second-biggest music retailer in America — second only to Walmart, fueled by iTunes and iPod sales”</p>'
+  document.getElementById('right-img').src = './Images/23. iTunes.JPG'
+}
+// --------------------------3
+document.getElementById('b4').addEventListener('click', change3)
+
+function change3 () {
+  document.getElementById('paragraph').innerHTML = '<p>24. Movie I<br><br>In 2013, the movie Jobs was released, starring Ashton Kutcher.</p>'
+  document.getElementById('right-img').src = './Images/24. Movie I.JPG'
+}
+// --------------------------4
+document.getElementById('b5').addEventListener('click', change4)
+
+function change4 () {
+  document.getElementById('paragraph').innerHTML = '<p>25. Movie II<br><br>In 2015, the movie “Steve Jobs” was released, it won the Golden Globe Award for Best Screenplay.</p>'
+  document.getElementById('right-img').src = './Images/15. Return to Apple.JPG'
+}
+// --------------------------5
+document.getElementById('b6').addEventListener('click', change5)
+
+function change5 () {
+  document.getElementById('paragraph').innerHTML = '<p>26. Success<br><br>“In 2018, Apple officially hit the $1 trillion mark, making it the first American Company to do so, ever” (Weinberger, Hartmans). He gave himself a $1 annual salary (Biography.com)(Shwarts).</p>'
+  document.getElementById('right-img').src = './Images/16. Apple is Profitable Again.JPG'
+}
+// --------------------------6
+document.getElementById('b7').addEventListener('click', change6)
+
+function change6 () {
+  document.getElementById('paragraph').innerHTML = '<p>27. iPhone<br><br>Apple introduced the first iPhone in 2007, and “owns 19.1 percent of the smartphone market as of August 2011” (CNET News Staff). <br><br>Jobs was “[i]nducted in the California Hall of Fame by Gov. Arnold Schwartzenegger” (CNET News Staff”.</p>'
+  document.getElementById('right-img').src = './Images/27. iPhone.JPG'
+}
+// --------------------------7
+document.getElementById('b8').addEventListener('click', change7)
+
+function change7 () {
+  document.getElementById('paragraph').innerHTML = '<p>28.Illness and Death<br><br>“January 17th 2011: Goes on medical leave of absence from CEO nearly two years after the six month break he took to undergo a liver transplant”, still he “makes a surprise appearance to take the wraps off the iPad 2” in March 2011. His last appearance at an Apple event took place on June 6th, 2011, and he pitched the “Cupertino on a new Apple campus.<br><br>August 9th, 2011, Apple shares edge past those of Exxon Mobil to become the most valuable U.S. company, in terms of market capitalization, at $337.17 billion. August 24th, 2011 [Jobs] resigns from Apple CEO post; becomes chairman (CNET News Staff). On Octer 5th, 2011: Apple Announces that Steve Jobs has passed away (Cammeroon).</p>'
+  document.getElementById('right-img').src = './Images/28.Illness and Death.JPG'
+}
+// --------------------------8
+document.getElementById('b9').addEventListener('click', change8)
+
+function change8 () {
+  document.getElementById('paragraph').innerHTML = '<p>29.Grief<br><br>“His death was followed by an outpouring of grief around the world from Apple fans and competitors, as well as heads of state.<br><br>In a sign of how pervasive the gadgets he spearheaded have become, much of the mourning was done on Apple gadgets: People held up pictures of candles on their iPads, reviewed his life on Macintosh Computers and tapped out tributes on iPhones” (Metz).</p>'
+  document.getElementById('right-img').src = './Images/29.Grief.JPG'
+}
+// --------------------------9
+document.getElementById('b10').addEventListener('click', change9)
+
+function change9 () {
+  document.getElementById('paragraph').innerHTML = '<p>20. Ad-War<br><br>From 2006 to 2009 Mac and PC engaged in an ‘ad-war’. Apples 2006 “Get a Mac” campaign started an ad-war that lasted until 2009 (Kolowich). </p>'
+  document.getElementById('right-img').src = './Images/20. Ad-War.JPG'
+}
+// --------------------------10
